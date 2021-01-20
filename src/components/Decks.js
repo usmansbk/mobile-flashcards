1974527fb755cff7cc0388a9ffcbb29eadd009d8
Usman Suleiman <usmansbk@gmail.com>
2021-01-20 20:59:52 +0100

    refactor: Add deck list view

---
+++ src/components/Decks.js
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as API from "../utils/api";
+
+export default function Decks() {
+  const decks = API.getDecks();
+  const data = Object.values(decks);
+
+  const onPressItem = () => null;
+  const onPressFAB = () => null;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Decks</Text>
+      <FlatList
+        data={data}
+        contentContainerStyle={styles.list}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={({ item }) => {
+          const { title, questions } = item;
+          return (
+            <TouchableOpacity
+              style={styles.itemContainer}
+              onPress={onPressItem}
+            >
+              <Text style={styles.itemTitle}>{title}</Text>
+              <View style={styles.cardCount}>
+                <MaterialCommunityIcons name="cards-outline" size={24} />
+                <Text style={styles.count}>{questions.length}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <FAB onPress={onPressFAB} />
+    </View>
+  );
+}
+
+const FAB_SIZE = 60;
+function FAB({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.fab} onPress={onPress}>
+      <MaterialCommunityIcons name="plus" size={FAB_SIZE / 2} color="white" />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  list: {
+    padding: 16,
+    paddingTop: 0,
+    paddingBottom: 100,
+  },
+  title: {
+    padding: 16,
+    fontWeight: "bold",
+    fontSize: 27,
+    color: "#000",
+  },
+  itemContainer: {
+    padding: 10,
+    elevation: 4,
+    backgroundColor: "white",
+    margin: 4,
+    borderRadius: 4,
+  },
+  itemTitle: {
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  cardCount: {
+    alignSelf: "flex-end",
+    flexDirection: "row",
+  },
+  count: {
+    fontSize: 20,
+    paddingHorizontal: 4,
+  },
+  fab: {
+    position: "absolute",
+    right: 0,
+    bottom: 24,
+    margin: 16,
+    width: FAB_SIZE,
+    height: FAB_SIZE,
+    borderRadius: FAB_SIZE / 2,
+    backgroundColor: "tomato",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 8,
+  },
+});
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Decks from "./Decks";
+
+export default function Screens() {
+  return <Decks />;
+}
