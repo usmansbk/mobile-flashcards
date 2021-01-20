8585ce395108f828256300a0d3b38b98e497ee2f
Usman Suleiman <usmansbk@gmail.com>
2021-01-20 20:19:29 +0100

    refactor: Add mock api

---
+++ src/utils/api.js
@@ -0,0 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const mock = {
+  React: {
+    title: "React",
+    questions: [
+      {
+        question: "What is React?",
+        answer: "A library for managing user interfaces",
+      },
+      {
+        question: "Where do you make Ajax requests in React?",
+        answer: "The componentDidMount lifecycle event",
+      },
+    ],
+  },
+  JavaScript: {
+    title: "JavaScript",
+    questions: [
+      {
+        question: "What is a closure?",
+        answer:
+          "The combination of a function and the lexical environment within which that function was declared.",
+      },
+    ],
+  },
+};
+
+export function getDecks() {
+  return mock;
+}
+
+export function getDeck(id) {
+  return mock[id];
+}
+
+export function saveDeckTitle(title) {
+  mock[title] = {
+    title,
+    questions: [],
+  };
+}
+
+export function addCardToDeck(title, card) {
+  mock[title].questions.push(card);
+}
